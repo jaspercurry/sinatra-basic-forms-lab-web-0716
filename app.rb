ed88066cdf47a990113de7451226ce2f8e62422e
jaspercurry <jaspercurry@gmail.com>
2016-08-11 23:30:16 -0400

    made puppy website :)

--- app.rb
+++ app.rb
@@ -2,4 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+  
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @age = params[:age]
+    @breed = params[:breed]
+    @name = params[:name]
+    erb :display_puppy
+  end
+
+
 end
